356a42be9e5da16c1a5b77504c219be8c1c49234
TimMcMullan <mcmullant@gmail.com>
2022-04-19 19:15:46 -0400

    initial setup and push

---
+++ index.js
@@ -0,0 +1,35 @@
+// TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+
+// TODO: Create an array of questions for user input
+// Lines above and below came with the starting code 
+// const questions = [];
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            message: 'What is the title of your project?',
+            name: 'projtitle',
+
+            type: 'input',
+            message: 'Give a description of your project.',
+            name: 'projdescription',
+
+            type: 'input',
+            messat: 'Please give installation instructions for your project',
+            name: 'installinstructions',
+
+            type: 'input',
+            message: 'Please explain how your project can be used to benefit the user.',
+            name: 'usageinfo',
+
+])
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) { }
+
+// TODO: Create a function to initialize app
+function init() { }
+
+// Function call to initialize app
+init();
